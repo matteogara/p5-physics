1c9d78768b52ee8506414768816dd17182a63f77
matteogara <matteo.garagiola@outlook.com>
2026-04-16 16:30:06 +0200

    pennelli colorati

--- brush.js
+++ brush.js
@@ -1,11 +1,37 @@
 class Brush {
   constructor(x, y, r) {
     this.body = Bodies.circle(x, y, r);
+    this.body.restitution = 1;
+    this.color = color(random(255), random(255), random(255));
   }
 
   draw() {
+    this.keepInBounds();
     noStroke();
-    fill(255, 0, 0);
+    fill(this.color);
     circle(this.body.position.x, this.body.position.y, this.body.circleRadius * 2);
   }
+
+  keepInBounds() {
+    let pos = this.body.position;
+    let r = this.body.circleRadius;
+
+        if(pos.x > width - r) {
+            Body.setPosition(this.body, {x: width - r, y: pos.y});
+            Body.setVelocity(this.body, {x: 0, y: this.body.velocity.y});
+        }
+        if(pos.x < r) {
+            Body.setPosition(this.body, {x: r, y: pos.y});
+            Body.setVelocity(this.body, {x: 0, y: this.body.velocity.y});
+        }
+        if(pos.y > height - r) {
+            Body.setPosition(this.body, {x: pos.x, y: height - r});
+            Body.setVelocity(this.body, {x: this.body.velocity.x, y: 0});
+        }
+        if(pos.y < r) {
+            Body.setPosition(this.body, {x: pos.x, y: r});
+            Body.setVelocity(this.body, {x: this.body.velocity.x, y: 0});
+        }
+    }
+
 }
--- sketch.js
+++ sketch.js
@@ -6,26 +6,39 @@ let Engine = Matter.Engine,
 let engine;
 
 let brush;
+
+let brushes = [];
+
 let obstacle;
+
+let NUM_BRUSHES = 100;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   engine = Engine.create();
 
-  brush = new Brush(width / 2, height / 4, 50);
+  for(let i = 0; i < NUM_BRUSHES; i++){
+    let size = random(10, 30);
+    brush = new Brush(width / 2, height / 4, size);
+    brushes.push(brush);
+    Composite.add(engine.world, brush.body);
+  }
+  
 
   obstacle = new Obstacle(width /2, 600, 300, 50, PI /6);
 
-  console.log(obstacle);
-
-  Composite.add(engine.world, [brush.body, obstacle.body]);
+  Composite.add(engine.world, obstacle.body);
 }
 
 function draw() {
-  background(220);
+  // background(220);
 
-  brush.draw();
+  for(let i = 0; i < brushes.length ; i++){
+    brushes[i].draw();
+  }
+  
+  // console.log(brush.body.speed);
 
   obstacle.draw();
 
